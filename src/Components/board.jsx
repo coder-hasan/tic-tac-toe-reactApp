5a8a408f334d1c896a69b1593900eaebfa3125f5
Mahmud Hasan <60518699+coder-hasan@users.noreply.github.com>
2020-04-30 11:58:42 +0600

    initilized this app

--- src/Components/board.jsx
+++ src/Components/board.jsx
@@ -2,22 +2,28 @@ import React from 'react'
 import Square from './square'
 
 function Board(props){
+
+    const renderSquare = i => <Square 
+        value ={props.squares[i]}
+        onClick={() => props.onClick(i)}
+    />
+
     return(
         <div>
             <div className="board-row">
-                <Square value="1"/>
-                <Square value="2"/>
-                <Square value="3"/>
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
             </div>
             <div className="board-row">
-                <Square value="1"/>
-                <Square value="2"/>
-                <Square value="3"/>
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
             </div>
             <div className="board-row">
-                <Square value="1"/>
-                <Square value="2"/>
-                <Square value="3"/>
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
             </div>
         </div>
     )
